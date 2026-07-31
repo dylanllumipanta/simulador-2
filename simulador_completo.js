d9dd57d5c0edebc467d56a36255aa576adfb8aa5
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-07-31 16:39:39 -0500

    funcion limpiar formulario

--- simulador_completo.js
+++ simulador_completo.js
@@ -34,57 +34,34 @@ function guardarTasa(){
             "La tasa debe estar entre 10% y 20%"
         );
     }
-
 }
 
-function guardarCliente(){
-    let cedula=recuperaraTexto("txtCedula");
-    let nombre=recuperaraTexto("txtNombre");
-    let apellido=recuperaraTexto("txtApellido");
-    let ingresos=recuperarFloat("txtIngresos");
-    let egresos=recuperarFloat("txtEgresos");
-    let cliente={
-        cedula:cedula,
-        nombre:nombre,
-        apellido:apellido,
-        ingresos:ingresos,
-        egresos:egresos
-    };
-    clientes.push(cliente);
-    pintarClientes();
-    limpiar();
-
-}
 
 function pintarClientes(){
-    let contenido="";
-    let cliente;
-    for(let i=0;i<clientes.length;i++){
-        cliente=clientes[i];
-        contenido+=`
-        <tr>
 
+    let contenido = "";
+
+    for(let i = 0; i < clientes.length; i++){
+
+        let cliente = clientes[i];
+
+        contenido += `
+        <tr>
             <td>${cliente.cedula}</td>
             <td>${cliente.nombre}</td>
             <td>${cliente.apellido}</td>
             <td>${cliente.ingresos}</td>
             <td>${cliente.egresos}</td>
-
             <td>
-
-            <button
-            onclick="seleccionarCliente('${cliente.cedula}')">
-            Actualizar
-            </button>
-
+                <button onclick="seleccionarCliente('${cliente.cedula}')">
+                    Actualizar
+                </button>
             </td>
-
         </tr>
         `;
-
     }
-    document.getElementById("tablaClientes").innerHTML=contenido;
 
+    document.getElementById("tablaClientes").innerHTML = contenido;
 }
 
 function buscarCliente(cedula){
@@ -123,16 +100,38 @@ function seleccionarCliente(cedula){
 }
   
 function guardarCliente(){
-    if(clienteSeleccionado==null){
+
+    if(clienteSeleccionado == null){
+
+        let cliente = {
+            cedula: recuperaraTexto("txtCedula"),
+            nombre: recuperaraTexto("txtNombre"),
+            apellido: recuperaraTexto("txtApellido"),
+            ingresos: recuperarFloat("txtIngresos"),
+            egresos: recuperarFloat("txtEgresos")
+        };
+
+        clientes.push(cliente);
+
     }else{
-        clienteSeleccionado.nombre=recuperaraTexto("txtNombre");
-        clienteSeleccionado.apellido=recuperaraTexto("txtApellido");
-        clienteSeleccionado.ingresos=recuperarFloat("txtIngresos");
-        clienteSeleccionado.egresos=recuperarFloat("txtEgresos");
-        clienteSeleccionado=null;
+
+        clienteSeleccionado.nombre = recuperaraTexto("txtNombre");
+        clienteSeleccionado.apellido = recuperaraTexto("txtApellido");
+        clienteSeleccionado.ingresos = recuperarFloat("txtIngresos");
+        clienteSeleccionado.egresos = recuperarFloat("txtEgresos");
+
+        clienteSeleccionado = null;
     }
+
     pintarClientes();
     limpiar();
-
 }
-//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+
+function limpiar(){
+    mostrarTextoEnCaja("txtCedula","");
+    mostrarTextoEnCaja("txtNombre","");
+    mostrarTextoEnCaja("txtApellido","");
+    mostrarTextoEnCaja("txtIngresos","");
+    mostrarTextoEnCaja("txtEgresos","");
+    clienteSeleccionado=null;
+}
