a5c7d158f2f99c370565b1670aed0c9100be0f60
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-07-31 16:14:38 -0500

    parte 1 lista

---
+++ simulador_completo.js
@@ -0,0 +1,24 @@
+
+  let clientes = [];
+  let creditos = [];
+
+  let tasaInteres = 15;
+  let clienteSeleccionado = null;
+  let cuotaCalculada = 0;
+  let montoCalculado = 0;
+  let plazoCalculado = 0;
+  let creditoAprobado = false;
+
+  function ocultarSecciones() {
+    document.getElementById("parametros").classList.remove("activa");
+    document.getElementById("clientes").classList.remove("activa");
+
+}
+
+function mostrarSeccion(id){
+    ocultarSecciones();
+    document.getElementById(id).classList.add("activa");
+}
+
+  
+//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
